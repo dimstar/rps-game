8b3ab4f7d38344d9e6571b8eae3be62b4a9904f7
dimstar <kylermagee@gmail.com>
2018-03-15 20:51:18 -0700

    created the ui for the RPS game

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,52 @@
 var database = firebase.database();
+
+var RPS = {
+    init: function(){
+
+    },
+    submitChoice: function(  ){
+
+    },
+    createPlayer: function( playerName ){
+
+    },
+    checkRndWinner: function( playerA, playerB){
+        var rndWinner = '';
+        if(playerA === playerB){
+            // tie
+            console.log("this is a tie");
+            rndWinner = false;
+            
+        }else if (playerA === "r"){
+            if(playerB === "p"){
+                console.log("playerB wins");
+                rndWinner = 'playerB';
+            }else{
+                console.log("playerA wins");
+                rndWinner = 'playerA';
+            }
+        }else if(playerA === "p"){
+            if(playerB === "r"){
+                console.log("playerA wins");
+                rndWinner = 'palyerA';
+            }else{
+                console.log("playerB wins");
+                rndWinner = 'playerB';
+            }
+        }else{
+            if(playerB == "r"){
+                console.log("playerB wins");
+                rndWinner = 'playerB';
+            }else{
+                console.log("playerA wins");
+                rndWinner = 'playerA';
+            }
+        }
+
+        return rndWinner;
+    }
+}
+
+RPS.init();
+
+/** EVENTS */
