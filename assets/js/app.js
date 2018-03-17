323eedf3a4ae620a2037cca3a309a39b03ce7423
dimstar <kylermagee@gmail.com>
2018-03-17 01:09:18 -0700

    added persistence to the players, and disconnection

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,22 @@ var RPS = {
     init: function(){
         
         RPS.foldUp();
+
+        /** EVENTS */
+        RPS.disconnectPlayer();
+
+        $('#addPlayer').on('click', function(){
+            var name = $('#playerNameInput');
+            RPS.makePlayer(name.val());
+            name.val('');
+        });
+
+        var rpsPlayers = firebase.database().ref('players');
+        rpsPlayers.on('value', function(snapshot) {
+        console.log(snapshot.val());
+        RPS.setPlayersLocal(snapshot.val());
+        RPS.startRound();
+        });
     },
     playHand: function(  ){
 
@@ -11,6 +27,33 @@ var RPS = {
     playersLocal: {
         player0: false,
         player1: false
+    },
+    currentPlayer: false,
+    disconnectPlayer: function(){
+        // set the current player first, we just refreshed.
+        this.getCurrentPlayer();
+        // destroy the player
+        firebase.database().ref('players/' + RPS.currentPlayer).remove();
+        
+        // kill the local storage and the rest
+        if(RPS.currentPlayer){
+            localStorage.removeItem(RPS.currentPlayer);
+            RPS.currentPlayer = false;
+            // send disconnect message
+        }
+    },
+    setCurrentPlayer: function( playerKey, playerName){
+        var playerMssg = $('<div>').attr('class', 'alert alert-success');
+        playerMssg.text(`Hi ${playerName}, you're ${playerKey}`);
+        $('#playerMssg').append(playerMssg);
+        localStorage.setItem(playerKey, playerName);
+    },
+    getCurrentPlayer: function(){
+        RPS.loopObect(RPS.playersLocal, RPS.returnStorage);
+    },
+    returnStorage: function(key){
+        var myName = localStorage.getItem(key);
+        if(myName) RPS.currentPlayer = key;
     },
     makePlayer: function( playerName ){
         // Set player key, either playerA or playerB
@@ -21,14 +64,14 @@ var RPS = {
             firebase.database().ref('players/' + newPlayerKey).set({
                 name: playerName
               });
+              // set the current player
+              RPS.setCurrentPlayer(newPlayerKey, playerName);
         }else{
             RPS.alertUi(`Sorry ${playerName} too many players!`);
         }
     },
     foldUp: function(){
-        $( ".rps-control" ).slideToggle( "slow", function() {
-            // Animation complete.
-          });
+        $( ".rps-control" ).slideToggle( "slow", function() {});
     },
     checkRndWinner: function( playerA, playerB){
         var rndWinner = '';
@@ -73,15 +116,46 @@ var RPS = {
         setTimeout(function(){ 
             $('#alertMssg').children('div').remove();
          }, 3000);
+    },
+    setupInputs: function(){
+        RPS.loopObect(RPS.playersLocal, RPS.bindControls);
+        
+        // bind events for clicking
+        $('.rps-control li').on('click', function(){
+            console.log('choice: ' + $(this).attr('data-choice') + ', player: ' + $(this).attr('data-player'));
+        });
+    },
+    setPlayersLocal: function(snapshotVal){
+        RPS.loopObect(snapshotVal, false, RPS.setPlayer);
+    },
+    loopObect: function(theObject, callOnKey = false, callOnBoth = false, callOnElement = false){
+        // Do crazy stuff :)
+        for (var aKey in theObject) {
+            if (theObject.hasOwnProperty(aKey)) {
+                var element = theObject[aKey];
+                if(callOnKey) callOnKey(aKey);
+                if(callOnBoth) callOnBoth(aKey, element);
+                if(callOnElement) callOnElement(element);
+            }
+        }
+    },
+    bindControls: function(playerKey){
+        $('#' + playerKey).find('.rps-control li').each(function(){
+            $(this).attr({
+                'data-player': playerKey,
+                'data-choice': $(this).text().toLowerCase()
+            });
+        });
+    },
+    setPlayer: function(playerKey, playerValues){
+        $('#' + playerKey + ' .card-header').html(playerValues.name);
+        RPS.playersLocal[playerKey] = true;
+    },
+    startRound: function(){
+        
     }
 }
 
 RPS.init();
 
-/** EVENTS */
 
-$('#addPlayer').on('click', function(){
-    var name = $('#playerNameInput');
-    RPS.makePlayer(name.val());
-    name.val('');
-});
